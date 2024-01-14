1dab6761f4ecbb66e57f1bf80bcbb4b657806b30
CodeSmitty <varcodesmitty@gmail.com>
2024-01-13 19:48:48 -0500

    removed length validation in spec/models/recipes.rb

--- spec/models/recipes.rb
+++ spec/models/recipes.rb
@@ -3,8 +3,6 @@ require 'rails_helper'
 
 RSpec.describe Recipe, :type => :model do
     it { is_expected.to validate_presence_of(:name)}
-    it { is_expected.to validate_length_of(:name)}
-    
     
 
 end
