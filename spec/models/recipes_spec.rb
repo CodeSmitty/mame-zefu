c3b917f41c8d3b69f9e9c8290ebef2c1d47027a0
nfoos <nfoos@rm-f.me>
2024-01-13 23:52:02 -0500

    rubocop

--- spec/models/recipes_spec.rb
+++ spec/models/recipes_spec.rb
@@ -1,8 +1,5 @@
 require 'rails_helper'
 
-
-RSpec.describe Recipe, :type => :model do
-    it { is_expected.to validate_presence_of(:name)}
-    
-
+RSpec.describe Recipe, type: :model do
+  it { is_expected.to validate_presence_of(:name) }
 end
