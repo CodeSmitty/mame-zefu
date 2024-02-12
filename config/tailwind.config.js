5ff3144ac3c19d54ad93bda99a705dc2c610eb6f
nfoos <nfoos@rm-f.me>
2024-02-12 07:53:07 -0500

    tom-select

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,8 +5,7 @@ module.exports = {
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}',
-    './node_modules/flowbite/**/*.js'
+    './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
     extend: {
@@ -19,7 +18,6 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-    require('flowbite/plugin')
+    require('@tailwindcss/container-queries')
   ]
 }
