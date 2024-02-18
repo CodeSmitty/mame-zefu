d56073155984618f863cba85a0e0ccb6b3242f60
CodeSmitty <varcodesmitty@gmail.com>
2024-02-18 11:32:50 -0500

    added styling to categories in the show page

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,6 @@
 class Recipe < ApplicationRecord
   validates :name, presence: true
   has_and_belongs_to_many :categories
-  accepts_nested_attributes_for :categories
 
   def category_names
     categories.pluck(:name)
