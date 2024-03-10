cd9610452e6cfa50bb7822ce520217dfe8ff6870
CodeSmitty <varcodesmitty@gmail.com>
2024-03-10 13:58:52 -0400

    Changed layout design and styling. Created auto height onchange for textarea fields

--- app/javascript/controllers/tom_select_controller.js
+++ app/javascript/controllers/tom_select_controller.js
@@ -2,12 +2,29 @@ import { Controller } from "@hotwired/stimulus"
 import TomSelect from 'tom-select'
 
 export default class extends Controller {
-  connect () {
-    new TomSelect(this.element ,{
-        plugins: ['remove_button'],
-        create: true,
-        createOnBlur: true,
-        persist: false,
+  connect() {
+
+    new TomSelect(this.element, {
+      plugins: ['remove_button', 'input_autogrow', 'change_listener'],
+      create: true,
+      createOnBlur: true,
+      persist: false,
+      itemClass: "category-items",
+      optionClass: "category-dropdown",
+      onItemAdd: function () {
+        this.setTextboxValue('');
+        this.refreshOptions();
+      }
     })
+
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+      // true for mobile device
+      const ts = document.getElementsByClassName("ts-control")
+      ts[0].setAttribute("style", "height: 70px !important")
+    } else {
+      const ts = document.getElementsByClassName("ts-control")
+      ts[0].setAttribute("style", "height: 40px !important")
+    }
   }
 }
+
