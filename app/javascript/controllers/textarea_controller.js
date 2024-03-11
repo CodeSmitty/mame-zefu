d963408f8fcd1c5b64aca9ecae323633b70eefcf
CodeSmitty <varcodesmitty@gmail.com>
2024-03-11 19:24:41 -0400

    changed file name and updated to cleaner  method for textarea resize

---
+++ app/javascript/controllers/textarea_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+    connect() {
+
+        this.initialHeight = this.element.offsetHeight
+        this.resizeElement(this.element)
+    }
+
+    resize(event) {
+        this.resizeElement(event.target)
+    }
+
+    resizeElement(element) {
+        element.style.height = this.initialHeight + "px";
+        element.style.height = element.scrollHeight + 3 + "px";
+    }
+}
