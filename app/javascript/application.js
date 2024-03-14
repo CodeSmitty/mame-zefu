d8df4efcd9dd8032b433c9280fda382a3ec6a200
nfoos <nfoos@rm-f.me>
2024-03-14 09:54:22 -0400

    Fix formatting issues

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import 'tom-select'
-import 'flowbite'
+import "tom-select"
+import "flowbite"
