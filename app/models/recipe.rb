9ac8af6e4ce67a6d79fc898bcbaf1eb6177b9856
CodeSmitty <varcodesmitty@gmail.com>
2024-03-28 07:35:58 -0400

    first commit of search input

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -10,5 +10,16 @@ class Recipe < ApplicationRecord
     self.category_ids = Category.from_names(category_names).pluck(:id)
   end
 
+  def self.search(params)
+    return all if params[:query].blank?
+
+    query = "%#{sanitize_sql_like(params[:query])}%"
+
+    where('name ILIKE ?', query)
+      .or(where('directions ILIKE ?', query))
+      .or(where('ingredients ILIKE ?', query))
+      .or(where('notes ILIKE ?', query))
+  end
+
   scope :sorted, -> { order(name: :asc) }
 end
