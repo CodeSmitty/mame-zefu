6ed51cf184a78139772b1096f888a53f38a54943
nfoos <nfoos@rm-f.me>
2025-02-13 19:49:34 -0500

    Add health check route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+  def health_check
+    render json: { ttl:, uptime:, ok: healthy? }, status:
+  end
+
+  private
+
+  def ttl
+    Uptime::TTL
+  end
+
+  def uptime
+    Time.current - Uptime::BOOTED_AT
+  end
+
+  def healthy?
+    uptime < ttl
+  end
+
+  def status
+    healthy? ? :ok : :service_unavailable
+  end
 end
