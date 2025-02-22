7af8e4528d82fffcf4c7be93fe45fae978f318c7
nfoos <nfoos@rm-f.me>
2025-02-22 16:07:55 -0500

    Add rubocop-rails and make it happy

--- spec/models/recipes_spec.rb
+++ spec/models/recipes_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe Recipe, type: :model do
   it { is_expected.to validate_presence_of(:name) }
 
-  let(:recipe) { Recipe.create(name: 'Spaghetti') }
-  let(:category_names) { %w[Pasta Italian Main] }
-
   describe '#category_names' do
     subject { recipe.category_names }
+
+    let(:recipe) { described_class.create(name: 'Spaghetti') }
+    let(:category_names) { %w[Pasta Italian Main] }
 
     context 'when recipe has no categories' do
       it { is_expected.to eq [] }
@@ -21,44 +21,49 @@ RSpec.describe Recipe, type: :model do
   end
 
   describe '#category_names=' do
-    subject { recipe.category_names = category_names }
+    subject(:described_method) { recipe.category_names = category_names }
+
+    let(:recipe) { described_class.create(name: 'Spaghetti') }
+    let(:category_names) { %w[Pasta Italian Main] }
 
     context 'when there are no categories' do
       it 'creates the categories' do
-        expect { subject }.to change { Category.count }.by(3)
+        expect { described_method }.to change(Category, :count).by(3)
       end
 
       it 'adds the categories to the recipe' do
-        expect { subject }.to change { recipe.categories.count }.by(3)
+        expect { described_method }.to change { recipe.categories.count }.by(3)
       end
     end
 
     context 'when the recipe already has categories' do
       before { recipe.categories << Category.from_names(existing_categories) }
+
       let(:existing_categories) { %w[Main Vegetarian] }
 
       it 'changes the categories to match the names exactly' do
-        expect(recipe.categories.map(&:name)).to match_array(existing_categories)
-        subject
-        expect(recipe.categories.map(&:name)).to match_array(category_names)
+        expect { described_method }
+          .to change { recipe.categories.map(&:name) }
+          .from(match_array(existing_categories))
+          .to match_array(category_names)
       end
 
       context 'when given an empty array' do
         let(:category_names) { [] }
 
         it 'removes all categories' do
-          expect { subject }.to change { recipe.categories.count }.from(2).to(0)
+          expect { described_method }.to change { recipe.categories.count }.from(2).to(0)
         end
       end
     end
   end
 
   describe '.search' do
-    subject { Recipe.search(query) }
+    subject { described_class.search(query) }
 
     let!(:recipes) do
       Array.new(3) do |i|
-        Recipe.create(name: "Recipe #{i}")
+        described_class.create(name: "Recipe #{i}")
       end
     end
 
@@ -75,7 +80,7 @@ RSpec.describe Recipe, type: :model do
     end
 
     context 'when query does not match case' do
-      let(:recipe) { Recipe.create(name: 'Birria Tacos') }
+      let(:recipe) { described_class.create(name: 'Birria Tacos') }
       let(:query) { 'taco' }
 
       it { is_expected.to include recipe }
