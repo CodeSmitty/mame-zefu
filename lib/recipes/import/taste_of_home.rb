8eea6eca9c84fe061c7c893cb9b34b2095ae44f8
codesmitty <varcodesmitty@gmail.com>
2025-03-06 05:10:18 +0000

    Changed taste of home to suggested simplified code.

--- lib/recipes/import/taste_of_home.rb
+++ lib/recipes/import/taste_of_home.rb
@@ -10,27 +10,22 @@ module Recipes
       def recipe_yield
         document
           .css('div.yield span')
-          .text[/(\d+ \w+)/, 1]
+          &.last
+          &.text
       end
 
       def recipe_prep_time
-        prep_time = total_time.index('Prep Time')
-        return nil unless prep_time
-
-        total_time[prep_time + 1]
+        document
+          .css('div.prep span')
+          &.last
+          &.text
       end
 
       def recipe_cook_time
-        cook_time = total_time.index('Cook Time')
-        return nil unless cook_time
-
-        total_time[cook_time + 1]
-      end
-
-      def recipe_total_time
         document
-          .css('div.time-unit')
-          .text[/(\d+ \w+)/, 1]
+          .css('div.cook span')
+          &.last
+          &.text
       end
 
       def recipe_ingredients
@@ -45,14 +40,6 @@ module Recipes
           .css('div.recipe-directions ol.recipe-directions__list li.recipe-directions__item')
           .map { |li| li.css('span').text.strip }
           .join("\n\n")
-      end
-
-      private
-
-      def total_time
-        document
-          .css('div.recipe-time span')
-          .map(&:text)
       end
     end
   end
