b5819b9e5fabba300598179b1c0d6f5f8cddd66e
codesmitty <varcodesmitty@gmail.com>
2025-03-11 06:36:27 +0000

    Fixed 92 rubo offenses with clearance spec generated files. Disabled metrics on visito_resets_password_spec.rb 51:

--- spec/features/clearance/visitor_resets_password_spec.rb
+++ spec/features/clearance/visitor_resets_password_spec.rb
@@ -1,7 +1,7 @@
-require "rails_helper"
-require "support/features/clearance_helpers"
+require 'rails_helper'
+require 'support/features/clearance_helpers'
 
-RSpec.feature "Visitor resets password" do
+RSpec.feature 'Visitor resets password' do
   before { ActionMailer::Base.deliveries.clear }
 
   around do |example|
@@ -11,23 +11,23 @@ RSpec.feature "Visitor resets password" do
     ActiveJob::Base.queue_adapter = original_adapter
   end
 
-  scenario "by navigating to the page" do
+  scenario 'by navigating to the page' do
     visit sign_in_path
 
-    click_link I18n.t("sessions.form.forgot_password")
+    click_link_or_button_or_button I18n.t('sessions.form.forgot_password')
 
-    expect(current_path).to eq new_password_path
+    expect(have_current_path).to eq new_password_path
   end
 
-  scenario "with valid email" do
+  scenario 'with valid email' do
     user = user_with_reset_password
 
     expect_page_to_display_change_password_message
     expect_reset_notification_to_be_sent_to user
   end
 
-  scenario "with non-user account" do
-    reset_password_for "unknown.email@example.com"
+  scenario 'with non-user account' do
+    reset_password_for 'unknown.email@example.com'
 
     expect_page_to_display_change_password_message
     expect_mailer_to_have_no_deliveries
@@ -39,16 +39,16 @@ RSpec.feature "Visitor resets password" do
     expect(user.confirmation_token).not_to be_blank
     expect_mailer_to_have_delivery(
       user.email,
-      "password",
-      user.confirmation_token,
+      'password',
+      user.confirmation_token
     )
   end
 
   def expect_page_to_display_change_password_message
-    expect(page).to have_content I18n.t("passwords.create.description")
+    expect(page).to have_content I18n.t('passwords.create.description')
   end
 
-  def expect_mailer_to_have_delivery(recipient, subject, body)
+  def expect_mailer_to_have_delivery(recipient, subject, body) # rubocop:disable Metrics/AbcSize
     expect(ActionMailer::Base.deliveries).not_to be_empty
 
     message = ActionMailer::Base.deliveries.any? do |email|
@@ -58,7 +58,7 @@ RSpec.feature "Visitor resets password" do
         email.text_part.body =~ /#{body}/
     end
 
-    expect(message).to be
+    expect(message).to
   end
 
   def expect_mailer_to_have_no_deliveries
