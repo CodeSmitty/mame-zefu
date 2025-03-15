d67654fec0cb411cd1bc2489f3d0c148040339e6
codesmitty <varcodesmitty@gmail.com>
2025-03-15 22:48:08 +0000

    resetting last working state

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,5 +1,5 @@
 Clearance.configure do |config|
-  config.routes = true
+  config.routes = false
   config.mailer_sender = ENV.fetch('TEST_SENDER_EMAIL', nil)
   config.rotate_csrf_on_sign_in = true
 end
