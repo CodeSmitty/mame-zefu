cf94d1837fb863e209f83052c18b61bee2959827
codesmitty <varcodesmitty@gmail.com>
2025-04-12 18:32:04 +0000

    fixed Recipe.delete_all in the AddUserRefToRecipe migraton

--- db/migrate/20250330181923_add_user_ref_to_recipes.rb
+++ db/migrate/20250330181923_add_user_ref_to_recipes.rb
@@ -1,6 +1,10 @@
 class AddUserRefToRecipes < ActiveRecord::Migration[7.2]
-  Recipe.delete_all
-  def change
+  def up
+    Recipe.delete_all
     add_reference :recipes, :user, null: false, foreign_key: true
+  end
+  
+  def down
+    remove_reference :recipes, :user, null: false, foreign_key: true
   end
 end
