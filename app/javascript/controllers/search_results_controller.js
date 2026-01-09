48fa3812251703925c3b718cdd9447d487a4e284
nfoos <nfoos@rm-f.me>
2026-01-09 16:51:45 -0500

    Update search results links (#62)

--- app/javascript/controllers/search_results_controller.js
+++ app/javascript/controllers/search_results_controller.js
@@ -2,34 +2,70 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
+    this._abort = new AbortController()
+
+    if (!this._onClickLink) this._onClickLink = this.onClickLink.bind(this)
+
     this.initializeObserver(this.element)
   }
 
-  initializeObserver(targetNode) {
-    const config = { attributes: false, childList: true, subtree: true }
-    this.searchResultsObserver = new MutationObserver(
-      this.searchResultsCallback,
-    )
-    this.searchResultsObserver.observe(targetNode, config)
+  onClickLink(e) {
+    const href = e.currentTarget.getAttribute("href")
+    if (href) {
+      e.preventDefault()
+
+      const params = new URLSearchParams({ url: href })
+      window.location.href = `web_result?${params}`
+    }
   }
 
-  searchResultsCallback(mutationList, observer) {
+  initializeObserver(targetNode) {
+    if (this._observer) return
+
+    if (!this._onMutations)
+      this._onMutations = this.searchResultsCallback.bind(this)
+
+    const config = { attributes: false, childList: true, subtree: true }
+    this._observer = new MutationObserver(this._onMutations)
+    this._observer.observe(targetNode, config)
+  }
+
+  searchResultsCallback(mutationList) {
     for (const mutation of mutationList) {
       if (mutation.type === "childList") {
         for (const node of mutation.addedNodes) {
-          if (
-            node.nodeType === Node.ELEMENT_NODE &&
-            node.matches("div.gsc-result")
-          ) {
-            const link = node.querySelector("div.gs-result a")
-            link.addEventListener("click", function (e) {
-              e.preventDefault()
+          if (!(node.nodeType === Node.ELEMENT_NODE)) continue
 
-              const params = new URLSearchParams({
-                url: this.getAttribute("href"),
-              })
-              window.location.href = `web_result?${params}`
-            })
+          if (node.matches("div.gsc-result")) {
+            const links = node.querySelectorAll("a")
+            for (const link of links) {
+              link.addEventListener("click", this._onClickLink)
+              link._boundToController = true
+            }
+          } else if (node.matches("div.gs-mobilePreview img.gs-imagePreview")) {
+            const link = node.parentElement
+            link.addEventListener("click", this._onClickLink)
+            link._boundToController = true
+          }
+        }
+
+        for (const node of mutation.removedNodes) {
+          if (!(node.nodeType === Node.ELEMENT_NODE)) continue
+
+          if (node.matches("div.gsc-result")) {
+            const links = node.querySelectorAll("a")
+            for (const link of links) {
+              if (!link._boundToController) continue
+
+              link.removeEventListener("click", this._onClickLink)
+              delete link._boundToController
+            }
+          } else if (node.matches("div.gs-mobilePreview img.gs-imagePreview")) {
+            const link = node.parentElement
+            if (!link._boundToController) continue
+
+            link.removeEventListener("click", this._onClickLink)
+            delete link._boundToController
           }
         }
       }
@@ -37,6 +73,7 @@ export default class extends Controller {
   }
 
   disconnect() {
-    this.searchResultsObserver.disconnect()
+    this._observer?.disconnect()
+    this._abort?.abort()
   }
 }
