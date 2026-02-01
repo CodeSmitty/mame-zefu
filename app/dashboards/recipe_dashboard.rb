9f8dfb2fca0b6fae8b0eb00475a176105f976fbf
nfoos <nfoos@rm-f.me>
2026-01-31 20:05:57 -0500

    customize dashboards

--- app/dashboards/recipe_dashboard.rb
+++ app/dashboards/recipe_dashboard.rb
@@ -1,105 +1,69 @@
-require "administrate/base_dashboard"
+require 'administrate/base_dashboard'
 
 class RecipeDashboard < Administrate::BaseDashboard
-  # ATTRIBUTE_TYPES
-  # a hash that describes the type of each of the model's fields.
-  #
-  # Each different type represents an Administrate::Field object,
-  # which determines how the attribute is displayed
-  # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
     id: Field::Number,
-    categories: Field::HasMany,
-    cook_time: Field::String,
-    description: Field::Text,
-    directions: Field::Text,
-    image_attachment: Field::HasOne,
-    image_blob: Field::HasOne,
-    ingredients: Field::Text,
-    is_favorite: Field::Boolean,
     name: Field::String,
-    notes: Field::Text,
-    prep_time: Field::String,
-    rating: Field::Number,
     source: Field::String,
-    user: Field::BelongsTo,
+    is_favorite: Field::Boolean,
+    rating: Field::Number,
     yield: Field::String,
+    prep_time: Field::String,
+    cook_time: Field::String,
+    categories: Field::HasMany,
+    description: Field::Text,
+    ingredients: Field::Text,
+    directions: Field::Text,
+    notes: Field::Text,
+    user: Field::BelongsTo,
     created_at: Field::DateTime,
-    updated_at: Field::DateTime,
+    updated_at: Field::DateTime
   }.freeze
 
-  # COLLECTION_ATTRIBUTES
-  # an array of attributes that will be displayed on the model's index page.
-  #
-  # By default, it's limited to four items to reduce clutter on index pages.
-  # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
     id
+    name
     categories
-    cook_time
-    description
+    created_at
   ].freeze
 
-  # SHOW_PAGE_ATTRIBUTES
-  # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
     id
-    categories
-    cook_time
-    description
-    directions
-    image_attachment
-    image_blob
-    ingredients
-    is_favorite
     name
-    notes
-    prep_time
-    rating
     source
-    user
+    is_favorite
+    rating
     yield
+    prep_time
+    cook_time
+    categories
+    description
+    ingredients
+    directions
+    notes
+    user
     created_at
     updated_at
   ].freeze
 
-  # FORM_ATTRIBUTES
-  # an array of attributes that will be displayed
-  # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-    categories
-    cook_time
-    description
-    directions
-    image_attachment
-    image_blob
-    ingredients
-    is_favorite
     name
-    notes
-    prep_time
-    rating
     source
-    user
+    is_favorite
+    rating
     yield
+    prep_time
+    cook_time
+    categories
+    description
+    ingredients
+    directions
+    notes
   ].freeze
 
-  # COLLECTION_FILTERS
-  # a hash that defines filters that can be used while searching via the search
-  # field of the dashboard.
-  #
-  # For example to add an option to search for open resources by typing "open:"
-  # in the search field:
-  #
-  #   COLLECTION_FILTERS = {
-  #     open: ->(resources) { resources.where(open: true) }
-  #   }.freeze
   COLLECTION_FILTERS = {}.freeze
 
-  # Overwrite this method to customize how recipes are displayed
-  # across all pages of the admin dashboard.
-  #
-  # def display_resource(recipe)
-  #   "Recipe ##{recipe.id}"
-  # end
+  def display_resource(recipe)
+    recipe.name
+  end
 end
