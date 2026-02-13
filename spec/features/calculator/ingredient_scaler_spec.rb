f5b4e9f092ee7249a587cc69cd270928ffdacecf
codesmitty <varcodesmitty@gmail.com>
2026-02-13 12:53:25 +0000

    ingredient_scaler coverage tests, still needs work

--- spec/features/calculator/ingredient_scaler_spec.rb
+++ spec/features/calculator/ingredient_scaler_spec.rb
@@ -19,6 +19,12 @@ RSpec.describe 'Ingredient Scaler' do
         expect(result[:scaled_description]).to eq '2/1 cup flour'
         expect(result[:scale_applied]).to be true
       end
+
+      it 'formats the scaled_description without unit' do
+        parsed_without_unit = { original: '1/2 sugar', quantity: Fractional.new('1/2'), unit: nil, ingredient: 'sugar' }
+        result = ingredient_scaler.scale_ingredient(parsed_without_unit, multiplier)
+        expect(result[:scaled_description]).to eq '1/1 sugar'
+      end
     end
 
     context 'when ingredient should not be scaled' do
@@ -32,6 +38,30 @@ RSpec.describe 'Ingredient Scaler' do
         expect(result[:scaled_description]).to eq 'A pinch of salt'
         expect(result[:scale_applied]).to be false
       end
+    end
+  end
+
+  describe '#scale_ingredients' do
+    subject(:scaled_results) { ingredient_scaler.scale_ingredients(parsed_ingredients, multiplier) }
+
+    let(:multiplier) { 2 }
+    let(:parsed_ingredients) do
+      [
+        { original: '1 cup flour', quantity: Fractional.new('1/1'), unit: 'cup', ingredient: 'flour' },
+        { original: '2 tablespoons butter', quantity: Fractional.new('2/1'), unit: 'tablespoons', ingredient: 'butter' },
+        { original: 'A pinch of salt', quantity: nil, unit: nil, ingredient: 'A pinch of salt' }
+      ]
+    end
+
+    it 'maps through parsed_ingredients and scales each one' do
+      results = scaled_results
+      
+      expect(results).to be_an(Array)
+      expect(results.length).to eq(3)
+      
+      expect(results[0][:scaled_quantity]).to eq('2/1')
+      expect(results[0][:scaled_description]).to eq('2/1 cup flour')
+      expect(results[0][:scale_applied]).to be true
     end
   end
 end
