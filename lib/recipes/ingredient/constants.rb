bee1c0fd3c253bb0f76a49978dd45025a39e27d9
nfoos <nfoos@rm-f.me>
2026-08-09 10:27:53 -0400

    Remove constants that aren't in use yet

--- lib/recipes/ingredient/constants.rb
+++ lib/recipes/ingredient/constants.rb
@@ -11,66 +11,6 @@ module Recipes
         '⅑' => '1/9',
         '⅒' => '1/10'
       }.freeze
-
-      VOLUME_UNITS = {
-        'tsp' => 1,
-        'tspn' => 1,
-        'teaspoon' => 1,
-        'teaspoons' => 1,
-        'tbsp' => 3,
-        'tbspn' => 3,
-        'tablespoon' => 3,
-        'tablespoons' => 3,
-        'cup' => 48,
-        'cups' => 48,
-        'qt' => 192,
-        'quart' => 192,
-        'quarts' => 192,
-        'gal' => 768,
-        'gallon' => 768,
-        'gallons' => 768
-      }.freeze
-
-      VOLUME_ORDER = %w[tsp tbsp cup qt gal].freeze
-
-      WEIGHT_UNITS = {
-        'g' => 1,
-        'gram' => 1,
-        'grams' => 1,
-        'kg' => 1000,
-        'kilogram' => 1000,
-        'kilograms' => 1000,
-        'pound' => 453.59237,
-        'pounds' => 453.59237,
-        'lb' => 453.59237,
-        'lbs' => 453.59237,
-        'ounce' => 28.3495,
-        'ounces' => 28.3495,
-        'oz' => 28.3495
-      }.freeze
-
-      WEIGHT_ORDER = %w[g oz lb kg].freeze
-
-      METRIC_WEIGHT_UNITS = %w[g gram grams kg kilogram kilograms].freeze
-      IMPERIAL_WEIGHT_UNITS = %w[oz ounce ounces lb lbs pound pounds].freeze
-      METRIC_WEIGHT_ORDER = %w[g kg].freeze
-      IMPERIAL_WEIGHT_ORDER = %w[oz lb].freeze
-
-      WEIGHT_LADDER_FOR_UNIT = {
-        'g' => METRIC_WEIGHT_ORDER,
-        'gram' => METRIC_WEIGHT_ORDER,
-        'grams' => METRIC_WEIGHT_ORDER,
-        'kg' => METRIC_WEIGHT_ORDER,
-        'kilogram' => METRIC_WEIGHT_ORDER,
-        'kilograms' => METRIC_WEIGHT_ORDER,
-        'oz' => IMPERIAL_WEIGHT_ORDER,
-        'ounce' => IMPERIAL_WEIGHT_ORDER,
-        'ounces' => IMPERIAL_WEIGHT_ORDER,
-        'lb' => IMPERIAL_WEIGHT_ORDER,
-        'lbs' => IMPERIAL_WEIGHT_ORDER,
-        'pound' => IMPERIAL_WEIGHT_ORDER,
-        'pounds' => IMPERIAL_WEIGHT_ORDER
-      }.freeze
     end
   end
 end
